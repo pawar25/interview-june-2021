da8ccf86370999903cfb3072c38a6308a76fff94
pawar25 <86229975+pawar25@users.noreply.github.com>
2021-06-22 12:53:51 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -1,9 +1,26 @@
 package exam;
-
-public class ConvertToBinary {
-
-  public String toBinary(final int n) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
-  }
+import java.io.*;`
+public class GFG
+{
+static void printBinary(int[] binary,int id)
+{
+for(int i=id-1;i>=0;i--)
+System.out.print(binary[i] +" ");
+}
+public static void decimalToBinary(int num)
+{
+int[] binary =new int[35];
+int id=0;
+while(num>0)
+{
+binary[id++]=num%2;
+num=num/2;
+}
+print Binary(binary,id);
+}
+public static void main(String[] args)
+{
+int num=45;
+decimalToBinary(num);
+}
 }
